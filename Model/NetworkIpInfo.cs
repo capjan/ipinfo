c5f4d0c9c49d0a5566ad032da4f6e84f94e42e5c
Jan Ruhländer <jan.ruhlaender@gmail.com>
2021-06-01 21:08:38 +0200

    build: restructured project files

--- Model/NetworkIpInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IpInfo.Model
-{
-    public class NetworkIpInfo
-    {
-        public string Ipv4 { get; init; } = string.Empty;
-        public string Ipv6 { get; init; } = string.Empty;
-        public string Name { get; init; } = string.Empty;
-    }
-}
